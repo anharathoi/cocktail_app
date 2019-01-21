400e7fd2835b1ba4b9382dcf96f2107ea5659f8c
guymcmanusgithub <guy@guymcmanus.com>
2019-01-21 16:51:44 +1100

    Initial Commit, including installing node, express, mongoose, nodemon, and creating the basic models and schema with database connection

---
+++ backend/api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = 5000;
+
+mongoose.connect('mongodb://localhost:27017/cocktail-app');
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongod');
+});
+
+mongoose.connection.on('error', () => {
+  console.log('failed to connect to mongod');
+});
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
